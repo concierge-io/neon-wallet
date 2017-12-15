8ff8bd2e984bc6c5046abe26bc3b488b82ee55ae
Chubby-Chocobo <nguyenhuuan@gmail.com>
2017-12-16 04:26:39 +0700

    Concierge color

--- app/components/Logo/Logo.jsx
+++ app/components/Logo/Logo.jsx
@@ -3,6 +3,6 @@ import React from 'react'
 import styles from './Logo.scss'
 import logo from '../../images/neon-logo2.png'
 
-const Logo = () => <div className={styles.logo}><img src={logo} width="130px" height="24px" /></div>
+const Logo = () => <div className={styles.logo}><img src={logo} width="130px" height="37px" /></div>
 
 export default Logo
